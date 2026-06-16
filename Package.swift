1b9cbabc5cdb5c31ddff16fc1d1f3d6ff3e87ac3
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-06-16 13:10:11 -0700

    Updating Package.swift to use InMobi SDK SPM package
    PiperOrigin-RevId: 933258227

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
-// Copyright 2025 Google LLC.
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,20 +22,25 @@ let package = Package(
   products: [
     .library(
       name: "InMobiAdapterTarget",
-      targets: ["InMobiAdapterTarget", "InMobi-iOS-SDK"]
+      targets: ["InMobiAdapterTarget"]
     )
   ],
   dependencies: [
     .package(
+      url: "https://github.com/InMobi/InMobiSDK-Swift-Package.git",
+      exact: "11.3.0"
+    ),
+    .package(
       url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
       from: "13.3.0"
-    )
+    ),
   ],
   targets: [
     .target(
       name: "InMobiAdapterTarget",
       dependencies: [
         .target(name: "InMobiAdapter"),
+        .product(name: "InMobiSDK", package: "InMobiSDK-Swift-Package"),
         .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
       ],
       path: "InMobiAdapterTarget"
@@ -45,12 +50,6 @@ let package = Package(
       url:
         "https://dl.google.com/googleadmobadssdk/mediation/ios/inmobi/InMobiAdapter-11.3.0.0.zip",
       checksum: "1fe26aebdb2ce67493e34d3a57a98d1f1005695a11fb356971195d3aac650457"
-    ),
-    .binaryTarget(
-      name: "InMobi-iOS-SDK",
-      url:
-        "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.3.0.zip",
-      checksum: "1dc4583ad8718845180903bfa6accc17c5248eca6d0aabbd43b9fb0dad1f58ce"
     ),
   ]
 )
